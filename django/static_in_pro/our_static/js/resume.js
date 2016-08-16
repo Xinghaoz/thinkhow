65bfe8505e2d4adbc9991ce9a70e34a99ce45aee
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-08-15 20:53:13 -0400

    Added Jquery

--- django/static_in_pro/our_static/js/resume.js
+++ django/static_in_pro/our_static/js/resume.js
@@ -1,24 +1,63 @@
-function myFunction() {
-    document.getElementById("name").style.display = "none";
-}
+// function myFunction() {
+//     document.getElementById("name").style.display = "none";
+// }
+//
+// function hideByClassName(className) {
+//     var elements = document.getElementsByClassName(className.toLowerCase());
+//     // elements[0].style.display = "none";
+//     for (var i = 0; i < elements.length; i++) {
+//         elements[i].style.display = "none";
+//         alert(elements[i].getAttribute("class"));
+//     }
+//     alert("Done!");
+// }
 
-function hideByClassName(className) {
-    var elements = document.getElementsByClassName(className.toLowerCase());
-    // elements[0].style.display = "none";
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = "none";
-        alert(elements[i].getAttribute("class"));
-    }
-    alert("Done!");
-}
+// function change(className) {
+//     var elements = document.getElementsByClassName(className.toLowerCase());
+//     for (var i = 0; i < elements.length; i++) {
+//         if (document.getElementById(className).checked) {
+//             elements[i].style.display = "block";
+//         } else {
+//             elements[i].style.display = "none";
+//         }
+//     }
+// }
 
-function change(className) {
-    var elements = document.getElementsByClassName(className.toLowerCase());
-    for (var i = 0; i < elements.length; i++) {
-        if (document.getElementById(className).checked) {
-            elements[i].style.display = "block";
-        } else {
-            elements[i].style.display = "none";
+$(document).ready(function() {
+    $("button").click(function() {
+        // var checked = $('input[type="checkbox"]:checked');
+        // if (checked.length > 0) {
+        //     checked.each(function() {
+        //         // alert("." + $(this).val());
+        //         $("." + $(this).val()).show();
+        //     })
+        // }
+
+        // var unchecked = $('input[type="checkbox"]'.prop("checked") == false) {
+        //     alert(unchecked.length);
+        // }
+        var boxes = $('input[type="checkbox"]');
+        if (boxes.length > 0) {
+            boxes.each(function() {
+                if ($(this).prop('checked')) {
+                    $("." + $(this).val()).show(777);
+                } else {
+                    $("." + $(this).val()).hide(777);
+                }
+            })
         }
-    }
-}
+    });
+
+    $(".checkbox").hover(function() {
+        $(this).css("color", "#cf8a05");
+        var category = $(this).text().trim().toLowerCase();
+        $("." + category).css("color", "#cf8a05");
+        $("." + category + " p").css("color", "#cf8a05");
+    },
+    function() {
+        $(this).css("color", "#000");
+        var category = $(this).text().trim().toLowerCase();
+        $("." + category).css("color", "#000");
+        $("." + category + " p").css("color", "#000");
+    });
+});
