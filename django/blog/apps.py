647b0d6ee1ae4fdb5b7db01b5c564da4e54f8bf5
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-09-26 23:29:46 -0400

    Add models and admin

--- django/blog/apps.py
+++ django/blog/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
-    name = 'blog'
+class GrumblrConfig(AppConfig):
+    name = 'grumblr'
--- django/blog/models.py
+++ django/blog/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    username = models.CharField(max_length=25)
+    text = models.CharField(max_length=42)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.timestamp)
