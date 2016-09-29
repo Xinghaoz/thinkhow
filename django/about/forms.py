fcf8af0e32df0069cfd12633d04954fa6484168a
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-09-28 22:36:22 -0400

    Add forms

---
+++ django/about/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from django.contrib.auth.models import User
+
+class StudentForm(forms.Form):
+    andrew_id = forms.CharField(max_length = 25)
+    first_name = forms.CharField(max_length = 50)
+    last_name = forms.CharField(max_length = 50)
+
+
+
+class CourseForm(forms.Form):
+    course_number = forms.CharField(max_length = 10)
+    course_name = forms.CharField(max_length = 50)
+    instructor = forms.CharField(max_length = 50)
+
+
+class RegistrationForm(forms.Form):
+    andirew_id = forms.CharField(max_length = 25)
+    course_number = forms.CharField(max_length = 10)
