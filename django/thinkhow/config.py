110f2c442fbd1c5112d08880a5bde4a52970fb2f
Xinghaoz <zhou.xinghao.1991@gmail.com>
2016-12-21 23:27:10 -0500

    Add config file

---
+++ django/thinkhow/config.py
@@ -0,0 +1,6 @@
+SECRET_KEY="a1egqzqj57bya!3mvj@l)e4v&^63y9)s)mj0h53pofb09$-fu-"
+username_email="yanjunl1"
+password_email="xiaocaideng110"
+username_database="postgres"
+password="110110"
+POSTGRE_PASSWORD='15637admin'
