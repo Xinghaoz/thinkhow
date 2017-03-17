ca017b9621572a9e1b285d12a5bb21abd28e9e62
Xinghaoz <zhou.xinghao.1991@gmail.com>
2017-03-16 23:48:53 -0700

    Add autoplay function

--- django/static_in_pro/our_static/js/home.js
+++ django/static_in_pro/our_static/js/home.js
@@ -315,10 +315,24 @@ var MyView = React.createClass({
     propTypes: {
         itemList: React.PropTypes.array.isRequired,
         hasMultipleItems: React.PropTypes.bool,
+        autoPlay:React.PropTypes.bool,
+        activeIndex:React.PropTypes.number,
+        defaultActiveIndex:React.PropTypes.number,
+        direction:React.PropTypes.oneOf['right','left'],
+    },
+    getDefaultProps(){
+    return{
+          interval:3000,
+          autoPlay:true,
+          defaultActiveIndex:0,
+          direction:'right',
+        }
     },
     getInitialState: function () {
         return {
             currentItemIndex: 0,
+            activeIndex:this.props.defaultActiveIndex?this.props.defaultActiveIndex:0,
+            direction:this.props.direction?this.props.direction:'right',
         };
     },
     onChildChanged: function (newState) {
@@ -326,6 +340,54 @@ var MyView = React.createClass({
             currentItemIndex: newState,
         });
     },
+    componentDidMount(){
+        this.autoPlay();
+    },
+    componentWillUnmount(){
+        clearInterval(this.timeOuter);
+    },
+    autoPlay(){
+        if(this.props.autoPlay){
+            if(this.props.direction==="right"){
+                this.timeOuter = setInterval(this.playRight,this.props.interval);
+            } else if (this.props.direction==="left"){
+                this.timeOuter = setInterval(this.playLeft,this.props.interval);
+            }
+        }
+    },
+    playRight(indexIn){
+        let index=indexIn?indexIn:this.state.activeIndex+1;
+        console.log(index);
+        if(index>this.props.number-1){
+            index=0;
+        }
+        this.setState({
+            activeIndex:index
+        })
+    },
+    playLeft(indexIn){
+        let index=indexIn?indexIn:this.state.activeIndex-1;
+        console.log(index);
+        if(index<0){
+            index=this.props.number-1;
+        }
+        this.setState({
+            activeIndex:index
+        })
+    },
+    left(){
+        clearInterval(this.timeOuter);
+        let oldIndex=this.props.activeIndex;
+        this.playLeft(oldIndex+1);
+        this.autoPlay();
+    },
+    right(){
+        clearInterval(this.timeOuter);
+        let oldIndex=this.props.activeIndex;
+        this.playRight(oldIndex-1);
+        this.autoPlay();
+    },
+
 
     render() {
         if (!this.props.hasMultipleItems) {
